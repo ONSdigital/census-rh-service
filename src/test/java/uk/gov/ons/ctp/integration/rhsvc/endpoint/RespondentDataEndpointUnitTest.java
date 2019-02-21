7d428c18fd2b908d272e8abee00fc630a7d72874
Martin Humphrey <Martin.Humphrey@ons.gov.uk>
2019-02-21 16:55:19 +0000

    Fix RespondentDataEndpointUnitTest

--- src/test/java/uk/gov/ons/ctp/integration/rhsvc/endpoint/RespondentDataEndpointUnitTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/rhsvc/endpoint/RespondentDataEndpointUnitTest.java
@@ -3,16 +3,11 @@ package uk.gov.ons.ctp.integration.rhsvc.endpoint;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static uk.gov.ons.ctp.common.MvcHelper.getJson;
 import static uk.gov.ons.ctp.common.utility.MockMvcControllerAdviceHelper.mockAdviceFor;
-
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -20,8 +15,6 @@ import uk.gov.ons.ctp.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
 
 /** Respondent Data Endpoint Unit tests */
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public final class RespondentDataEndpointUnitTest {
   @InjectMocks private RespondentDataEndpoint respondentEndpoint;
 
