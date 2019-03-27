b3401939a0ec79349b9ed056dc6cd43c91728cac
cookel2 <eleanor.cook@ons.gov.uk>
2019-03-27 12:03:28 +0000

    Add a CustomMessageListener that can receive a generic message and show its properties as a String

--- src/main/java/uk/gov/ons/ctp/integration/rhsvc/RHSvcApplication.java
+++ src/main/java/uk/gov/ons/ctp/integration/rhsvc/RHSvcApplication.java
@@ -1,8 +1,12 @@
 package uk.gov.ons.ctp.integration.rhsvc;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.integration.annotation.IntegrationComponentScan;
@@ -25,5 +29,17 @@ public class RHSvcApplication {
   public static void main(final String[] args) {
 
     SpringApplication.run(RHSvcApplication.class, args);
+  }
+
+  @Bean
+  public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+    final var rabbitTemplate = new RabbitTemplate(connectionFactory);
+    rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+    return rabbitTemplate;
+  }
+
+  @Bean
+  public Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+    return new Jackson2JsonMessageConverter();
   }
 }
