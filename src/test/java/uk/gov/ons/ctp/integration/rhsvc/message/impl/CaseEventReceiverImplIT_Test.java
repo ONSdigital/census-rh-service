33b774c2c8665a8eb7520e2630879f5f53cd80f2
Phil Whiles <phil.whiles@gmail.com>
2019-05-01 10:49:47 +0100

    IT tests now mock receiver and its use of GCS

--- src/test/java/uk/gov/ons/ctp/integration/rhsvc/message/impl/CaseEventReceiverImplIT_Test.java
+++ src/test/java/uk/gov/ons/ctp/integration/rhsvc/message/impl/CaseEventReceiverImplIT_Test.java
@@ -14,6 +14,7 @@ import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import uk.gov.ons.ctp.integration.rhsvc.domain.model.Address;
@@ -32,26 +33,24 @@ public class CaseEventReceiverImplIT_Test {
 
   @Autowired private CaseEventReceiverImpl receiver;
   @Autowired private SimpleMessageListenerContainer caseEventListenerContainer;
-  @Autowired private RespondentDataServiceImpl respondentDataServiceImpl;
+  @MockBean private RespondentDataServiceImpl respondentDataServiceImpl;
 
   /** Test the receiver flow */
   @Test
   public void caseEventFlowTest() throws Exception {
 
-    String bucket = "case_bucket";
-
     // Construct CaseEvent
     CaseEvent caseEvent = new CaseEvent();
     CasePayload casePayload = caseEvent.getPayload();
     CollectionCase collectionCase = casePayload.getCollectionCase();
-    Address address = collectionCase.getAddress();
-    Contact contact = collectionCase.getContact();
     collectionCase.setId("900000000");
     collectionCase.setCaseRef("10000000010");
     collectionCase.setSurvey("Census");
     collectionCase.setCollectionExerciseId("n66de4dc-3c3b-11e9-b210-d663bd873d93");
     collectionCase.setState("actionable");
     collectionCase.setActionableFrom("2011-08-12T20:17:46.384Z");
+
+    Address address = collectionCase.getAddress();
     address.setAddressLine1("1 main street");
     address.setAddressLine2("upper upperingham");
     address.setAddressLine3("");
@@ -64,11 +63,14 @@ public class CaseEventReceiverImplIT_Test {
     address.setArid("XXXXX");
     address.setAddressType("CE");
     address.setEstabType("XXX");
+
+    Contact contact = collectionCase.getContact();
     contact.setTitle("Ms");
     contact.setForename("jo");
     contact.setSurname("smith");
     contact.setEmail("me@example.com");
     contact.setTelNo("+447890000000");
+
     Header header = new Header();
     header.setType("CASE_UPDATED");
     header.setTransactionId("c45de4dc-3c3b-11e9-b210-d663bd873d93");
@@ -89,8 +91,5 @@ public class CaseEventReceiverImplIT_Test {
     ArgumentCaptor<CaseEvent> captur = ArgumentCaptor.forClass(CaseEvent.class);
     verify(receiver).acceptCaseEvent(captur.capture());
     assertTrue(captur.getValue().getPayload().equals(caseEvent.getPayload()));
-
-    // Tidy up by deleting the CaseEvent message from the case_bucket
-    respondentDataServiceImpl.deleteJsonFromCloud("900000000", bucket);
   }
 }
