3f7f52e342472316bcf59b3c6f304ebeb2f2fd6a
cookel2 <eleanor.cook@ons.gov.uk>
2019-05-13 14:03:05 +0100

    change pom to use version 0.0.18 of census-int-common framework and fix resulting import problems

--- src/main/java/uk/gov/ons/ctp/integration/rhsvc/message/RespondentEventPublisher.java
+++ src/main/java/uk/gov/ons/ctp/integration/rhsvc/message/RespondentEventPublisher.java
@@ -1,7 +1,5 @@
 package uk.gov.ons.ctp.integration.rhsvc.message;
 
-import uk.gov.ons.ctp.common.message.GenericEvent;
-
 /**
  * Service responsible for the publication of respondent events to the Response Management System.
  */
