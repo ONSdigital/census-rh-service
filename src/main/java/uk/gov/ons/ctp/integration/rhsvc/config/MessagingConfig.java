84163a273ed9981201e67bfcc0606463adc53215
Rob Catling <github@rpc-soft.com>
2020-10-20 10:10:49 +0100

    CR-1190 javadoc to clarify the backoffs

--- src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/MessagingConfig.java
+++ src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/MessagingConfig.java
@@ -5,8 +5,12 @@ import lombok.Data;
 @Data
 public class MessagingConfig {
   private boolean mismatchedQueuesFatal;
+
+  /* processingBackoff - retrying when an event fails to be processed with rabbit working. */
   private BackoffConfig processingBackoff;
+  /* recoveryBackoff - retrying the listener when rabbit stops working. */
   private BackoffConfig recoveryBackoff;
+
   private int consumingThreads;
   private int conMaxAttempts;
   private int prefetchCount;
