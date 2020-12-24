bf463b8f373178b39d5a273817ec013e610a90f6
Rob Catling <github@rpc-soft.com>
2020-12-24 10:24:50 +0000

    CR-1459 add clientIP to survey DTO and update endpoint unit tests

--- src/test/java/uk/gov/ons/ctp/integration/rhsvc/endpoint/SurveyLaunchedEndpointUnitTest.java
+++ src/test/java/uk/gov/ons/ctp/integration/rhsvc/endpoint/SurveyLaunchedEndpointUnitTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static uk.gov.ons.ctp.common.MvcHelper.postJson;
 import static uk.gov.ons.ctp.common.utility.MockMvcControllerAdviceHelper.mockAdviceFor;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,8 +16,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import uk.gov.ons.ctp.common.FixtureHelper;
 import uk.gov.ons.ctp.common.error.RestExceptionHandler;
 import uk.gov.ons.ctp.common.jackson.CustomObjectMapper;
+import uk.gov.ons.ctp.integration.rhsvc.representation.SurveyLaunchedDTO;
 import uk.gov.ons.ctp.integration.rhsvc.service.SurveyLaunchedService;
 
 /** Respondent Home Endpoint Unit tests */
@@ -26,7 +29,10 @@ public final class SurveyLaunchedEndpointUnitTest {
 
   @Mock SurveyLaunchedService surveyLaunchedService;
 
+  private ObjectMapper mapper = new ObjectMapper();
+
   private MockMvc mockMvc;
+  private SurveyLaunchedDTO dto;
 
   /**
    * Set up of tests
@@ -40,56 +46,57 @@ public final class SurveyLaunchedEndpointUnitTest {
             .setHandlerExceptionResolvers(mockAdviceFor(RestExceptionHandler.class))
             .setMessageConverters(new MappingJackson2HttpMessageConverter(new CustomObjectMapper()))
             .build();
+    dto = FixtureHelper.loadClassFixtures(SurveyLaunchedDTO[].class).get(0);
   }
 
   @Test
   public void surveyLaunchedSuccessCaseEmptyString() throws Exception {
     Mockito.doNothing().when(surveyLaunchedService).surveyLaunched(any());
-
-    String surveyLaunchedRequestBody =
-        "{ \"questionnaireId\": \"23434234234\", "
-            + "\"caseId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\", "
-            + "\"agentId\": \"\"}";
-    mockMvc
-        .perform(postJson("/surveyLaunched", surveyLaunchedRequestBody))
-        .andExpect(status().isOk());
+    callEndointExpectingSuccess();
   }
 
   @Test
   public void surveyLaunchedSuccessCaseAssistedDigitalLocation() throws Exception {
     Mockito.doNothing().when(surveyLaunchedService).surveyLaunched(any());
+    callEndointExpectingSuccess();
+  }
 
+  @Test
+  public void shouldRejectMissingQuestionnaireId() throws Exception {
+    dto.setQuestionnaireId(null);
+    callEndointExpectingBadRequest();
+  }
+
+  @Test
+  public void shouldLaunchWithMissingClientIP() throws Exception {
+    dto.setClientIP(null);
+    callEndointExpectingSuccess();
+  }
+
+  @Test
+  public void shouldRejectInvalidCaseId() throws Exception {
     String surveyLaunchedRequestBody =
-        "{ \"questionnaireId\": \"23434234234\", "
-            + "\"caseId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\", "
-            + "\"agentId\": \"1000007\"}";
+        "{ \"questionnaireId\": \"23434234234\",   \"caseId\": \"euieuieu@#$@#$\" }";
+    callEndointExpectingBadRequest(surveyLaunchedRequestBody);
+  }
+
+  @Test
+  public void shouldRejectInvalidRequest() throws Exception {
+    String surveyLaunchedRequestBody = "uoeuoeu 45345345 euieuiaooo";
+    callEndointExpectingBadRequest(surveyLaunchedRequestBody);
+  }
+
+  private void callEndointExpectingSuccess() throws Exception {
     mockMvc
-        .perform(postJson("/surveyLaunched", surveyLaunchedRequestBody))
+        .perform(postJson("/surveyLaunched", mapper.writeValueAsString(dto)))
         .andExpect(status().isOk());
   }
 
-  @Test
-  public void surveyLaunchedWithMissingQuestionnaireId() throws Exception {
-    String surveyLaunchedRequestBody = "{ \"caseId\": \"3fa85f64-5717-4562-b3fc-2c963f66afa6\" }";
-    mockMvc
-        .perform(postJson("/surveyLaunched", surveyLaunchedRequestBody))
-        .andExpect(status().isBadRequest());
+  private void callEndointExpectingBadRequest() throws Exception {
+    callEndointExpectingBadRequest(mapper.writeValueAsString(dto));
   }
 
-  @Test
-  public void surveyLaunchedWithInvalidCaseId() throws Exception {
-    String surveyLaunchedRequestBody =
-        "{ \"questionnaireId\": \"23434234234\",   \"caseId\": \"euieuieu@#$@#$\" }";
-    mockMvc
-        .perform(postJson("/surveyLaunched", surveyLaunchedRequestBody))
-        .andExpect(status().isBadRequest());
-  }
-
-  @Test
-  public void surveyLaunchedWithInvalidRequest() throws Exception {
-    String surveyLaunchedRequestBody = "uoeuoeu 45345345 euieuiaooo";
-    mockMvc
-        .perform(postJson("/surveyLaunched", surveyLaunchedRequestBody))
-        .andExpect(status().isBadRequest());
+  private void callEndointExpectingBadRequest(String body) throws Exception {
+    mockMvc.perform(postJson("/surveyLaunched", body)).andExpect(status().isBadRequest());
   }
 }
