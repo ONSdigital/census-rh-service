0c69f7309970e29d218dd37454343ea27faec4ee
Rob Catling <github@rpc-soft.com>
2021-01-05 17:01:56 +0000

    CR-1459 add validation to prevent zero load shedding modulus

--- src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/AppConfig.java
+++ src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/AppConfig.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.retry.annotation.EnableRetry;
+import org.springframework.validation.annotation.Validated;
 import uk.gov.ons.ctp.common.config.CustomCircuitBreakerConfig;
 
 /** Application Config bean */
 @EnableRetry
+@Validated
 @Configuration
 @ConfigurationProperties
 @Data
--- src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/LoadsheddingConfig.java
+++ src/main/java/uk/gov/ons/ctp/integration/rhsvc/config/LoadsheddingConfig.java
@@ -1,8 +1,11 @@
 package uk.gov.ons.ctp.integration.rhsvc.config;
 
+import javax.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class LoadsheddingConfig {
+
+  @Min(1)
   private int modulus;
 }
